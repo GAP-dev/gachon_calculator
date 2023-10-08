d50ece21e3e47b152a93d22c285f29d0d8812c0a
leedongha <gapdev2004@gmail.com>
2023-10-08 17:45:12 +0900

    이형균 커밋

--- gachon_calculator.c
+++ gachon_calculator.c
@@ -24,17 +24,34 @@ int multiply(int a, int b) {
 }
 
 int divide(int a, int b) {
-    int calc;
-
-
+    int m = 0;
+    if (b != 0) {
+        while (a >= b) {
+            a -= b;
+            m++;
+        }
+        return m;
+    }    else {
+        printf("0¿∏∑Œ ≥™¥≠ ºˆ æ¯Ω¿¥œ¥Ÿ.\n");
+        return 0;
+    }
     return calc;
 }
 
 int remainder(int a, int b) {
     int calc;
-
+    while (a >= b) {
+        a -= b;
+    }
+    return a;
 
     return calc;
+}
+
+void Binary(int n) {
+    for (int i = sizeof(int) * 8 - 1; i >= 0; i--) {
+        printf("%d", (n >> i) & 1);
+    }
 }
 
 
@@ -44,6 +61,7 @@ int main(void)
 	int y;
 	char op;
     int result;
+    int r;
 
 	printf("계산할 식을 입력하세요 >> ");
 	scanf("%d %c %d", &x, &op, &y);
@@ -62,7 +80,15 @@ int main(void)
         break;
     }
 
-    printf("%d%c%d = %d [HEX: %X, OCT: %o, BIN: %d]\n", x, op, y, result, result, result, result);
+    if (op == '/') {
+        printf("%d%c%d = %d [≥™∏”¡ˆ¥¬ %d] [HEX: %X, OCT: %o, BIN: ]", x, op, y, result, r, result, result, result);
+        Binary(result);
+    }
+    else {
+        printf("%d%c%d = %d [HEX: %X, OCT: %o, BIN: ", x, op, y, result, result, result);
+        Binary(result);
+        printf("]\n");
+    }
     
 	return 0;
 }
